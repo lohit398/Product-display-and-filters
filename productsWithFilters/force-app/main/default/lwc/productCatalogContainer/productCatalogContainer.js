75d2a7559377622c8effba7b82719c0fec0fb0ca
Lohit Botta <lohit.b@lohitb-ltm.internal.salesforce.com>
2020-12-02 18:00:28 +0530

    Product Catalog, filters and redirection

---
+++ productsWithFilters/force-app/main/default/lwc/productCatalogContainer/productCatalogContainer.js
@@ -0,0 +1,69 @@
+import { LightningElement,wire,track } from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+
+import FAMILY from '@salesforce/schema/Product2.Family';
+
+export default class ProductCatalogContainer extends LightningElement {
+    options = [{ label: 'Overheden', value: 'Overheden' },
+    { label: 'Bedrijven', value: 'Bedrijven' },
+    { label: 'Aannemers', value: 'Aannemers' },
+    { label: 'Particulieren', value: 'Particulieren' }];
+
+    options1 = [{ label: 'Bebouwing', value: 'Bebouwing' },
+    { label: 'Bedrijvengebied', value: 'Bedrijvengebied' },
+    { label: 'Bodem', value: 'Bodem' },
+    { label: 'Natuur', value: 'Natuur' },
+    { label: "Publieke ruimte", value: "Publieke ruimte " },
+    { label: "Water", value: "Water " },
+    { label: 'Wegen', value: 'Wegen' }];
+
+    options2 = [{ label: 'Aanleg & realisatie', value: 'Aanleg & realisatie' },
+    { label: 'Doorstroming', value: 'Doorstroming' },
+    { label: 'Droge voeten', value: 'Droge voeten' },
+    { label: 'Duurzaamheid', value: 'Duurzaamheid' },
+    { label: 'Energie', value: 'Energie' },
+    { label: 'Onderhoud', value: 'Onderhoud' },
+    { label: 'Overlast reductie', value: 'Overlast reductie' },
+    {label:'Veiligheid',value:'Veiligheid'}];
+
+    value = [""];
+
+    productFamily=["All"];
+    productFamilies;
+    productFamilyFilters = [];
+    themaFilters = [];
+    Loca_tie_filters = [];
+    Oplossingen_voor_filters = [];
+    filters={};
+    filterString = '';
+
+    @wire(getPicklistValues,{recordTypeId:'01209000001A021AAC',fieldApiName: FAMILY })
+    getPicklistDetails({error,data}){
+        if(data){
+            this.productFamilies = JSON.parse(JSON.stringify(data.values));
+            this.productFamilies.unshift({label:'All',value:'All'});
+        }
+        if(error){
+            console.log(error);
+        }
+    }
+    handleFilterChange(event){
+        if(event.target.dataset.filtername === 'Family'){
+            this.productFamilyFilters = event.detail.value;
+            this.filters.productFamilyFilters = this.productFamilyFilters;
+        }   
+        else if(event.target.dataset.filtername === 'Thema'){
+            this.themaFilters = event.detail.value;
+            this.filters.themaFilters = this.themaFilters;
+        }
+        else if(event.target.dataset.filtername === 'Oplossingen_voor'){
+            this.Oplossingen_voor_filters = event.detail.value;
+            this.filters.Oplossingen_voor_filters = this.Oplossingen_voor_filters;
+        }
+        else if(event.target.dataset.filtername === 'Loca_tie'){
+            this.Loca_tie_filters = event.detail.value;
+            this.filters.Loca_tie_filters = this.Loca_tie_filters;
+        }
+        this.filterString = JSON.stringify(this.filters);
+    }
+}
---
+++ productsWithFilters/force-app/main/default/lwc/productDisplay/productDisplay.js
@@ -0,0 +1,79 @@
+import { LightningElement, wire, track, api } from 'lwc';
+import GET_PRODUCTS from '@salesforce/apex/DE_GetProducts.getProducts';
+import FILTER_PRODUCTS from '@salesforce/apex/DE_GetProducts.filterProducts';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class ProductDisplay extends NavigationMixin(LightningElement) {
+
+    filterValues = {};
+    allProducts = [];
+    @track products = [];
+
+
+    @wire(GET_PRODUCTS)
+    getProducts({ error, data }) {
+        if (error) {
+            console.log(error)
+        }
+        else if (data) {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].Image_URL__c != undefined && data[i].Image_URL__c != null)
+                    this.products.push(data[i]);
+                this.allProducts.push(data[i]);
+            }
+
+        }
+    }
+
+    @api
+    get filters() {
+        return this.filterValues;
+    }
+
+    set filters(value) {
+        if(value != '')
+            this.filterValues = JSON.parse(value);
+
+        /*console.log(this.filterValues.productFamilyFilters);
+        console.log(this.filterValues.themaFilters);
+        console.log(this.filterValues.Oplossingen_voor_filters);
+        console.log(this.filterValues.Loca_tie_filters)*/
+
+        if (this.filterValues.productFamilyFilters != undefined) {
+            if (this.filterValues.productFamilyFilters.indexOf('All') === -1) {
+                FILTER_PRODUCTS({ 
+                    productFamilyFilters: this.filterValues.productFamilyFilters,
+                    themaFilters: this.filterValues.themaFilters,
+                    Oplossingen_voor_filters: this.filterValues.Oplossingen_voor_filters,
+                    Loca_tie_filters: this.filterValues.Loca_tie_filters
+                })
+                    .then(response => {
+                        this.products = [];
+                        for (let i = 0; i < response.length; i++) {
+                            if (response[i].Image_URL__c != undefined && response[i].Image_URL__c != null)
+                                this.products.push(response[i]);
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            }
+            else {
+                this.products = JSON.parse(JSON.stringify(this.allProducts));
+            }
+        }
+    }
+
+
+    navigateNext(event) {
+        //console.log(event.currentTarget.dataset.recordid);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: event.currentTarget.dataset.recordid,
+                objectApiName: 'Product2',
+                actionName: 'view'
+            },
+        });
+    }
+}
